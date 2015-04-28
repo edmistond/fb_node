e8225adeca4c071f53c18fdc5b471e730e2cee88
David Edmiston <edmistond@gmail.com>
2015-04-28 14:42:33 -0400

    test

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ var _ = require('lodash');
 var apiUrl = 'http://localhost:3002/';
 
 function Feedbin(username, password) {
-  this.username = username;
-  this.password = password;
+    console.log('received username {username} and password ${password}');
+    this.username = username;
+    this.password = password;
 }
 
-Feedbin.prototype.subscriptions = function() {};
+Feedbin.prototype.subscriptions = function(callback) {
+    request('http://localhost:3002/subscriptions.json/', function(err, res, body) {
+        if (!err && response.statusCode === 200) {
+            callback(null, body);
+        } else {
+            callback(err, null);
+        }
+    });
+};
+
+module.exports = Feedbin;
