3c71b96468461024ca7eed5b9a174b069e46d7b0
David Edmiston <edmistond@gmail.com>
2015-04-28 23:31:52 -0400

    Updated testing file

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-var fb = require('./index')(null,null);
+var Feedbin = require('./index');
+var _ = require('lodash');
+var async = require('async');
+
+var readyFunc = function() {
+  // console.log(fb.getSubcriptionsForTag('mac-tech').length);
+  // console.log(fb.getSubcriptionsForTag());
+
+  _.forEach(fb.tagNames, function(t) {
+    console.log('current tag:', t);
+    _.forEach(fb.getSubcriptionsForTag(t), function(sub) {
+      console.log("\t", sub.title, sub.tags);
+    });
+    console.log("\n\n");
+  });
+};
+
+var fb = new Feedbin('edmistond', 'pass', 'http://localhost:3002', readyFunc);
