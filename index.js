eb1a02c88c9193d9c479cad37c9b4a249dd5274f
David Edmiston <edmistond@gmail.com>
2015-04-28 23:31:21 -0400

    Rewrote index.js to use async and added features

--- index.js
+++ index.js
@@ -1,21 +1,93 @@
 var request = require('request');
 var _ = require('lodash');
-var apiUrl = 'http://localhost:3002/';
+var async = require('async');
 
-function Feedbin(username, password) {
-    console.log('received username {username} and password ${password}');
-    this.username = username;
-    this.password = password;
-}
+var Feedbin = function(un, pw, url, ready) {
+  var self = this;
+  console.log('received username: ' + un + ' and password: ' + pw);
+  self.username = un;
+  self.password = pw;
+  self.apiBaseUrl = url;
 
-Feedbin.prototype.subscriptions = function(callback) {
-    request('http://localhost:3002/subscriptions.json/', function(err, res, body) {
-        if (!err && response.statusCode === 200) {
-            callback(null, body);
-        } else {
-            callback(err, null);
-        }
+  self.allSubs = [];
+  self.allTags = [];
+  self.tagNames = [];
+
+  self.init = function(ready) {
+    async.series({
+      subs: getSubscriptions,
+      tags: getTags
+    }, function(err, results) {
+      self.allSubs = results.subs;
+      self.allTags = results.tags.tags;
+      self.tagNames = results.tags.tagNames;
+
+      mergeSubsAndTags();
+
+      ready();
     });
+  };
+
+  self.getSingleSubByAttributeObject = function(attrib) {
+    return _.chain(self.allSubs)
+      .filter(attrib)
+      .first()
+      .value();
+  };
+
+  self.getSubcriptionsForTag = function(tag) {
+    return _.select(self.allSubs, function(sub) {
+      if (_.isEmpty(tag))
+        return _.isEmpty(sub.tags);
+      else
+        return _.includes(sub.tags, tag);
+    });
+  };
+
+  var mergeSubsAndTags = function() {
+    _.forEach(self.allSubs, function(sub, index) {
+      self.allSubs[index].tags = _.chain(self.allTags)
+        .filter({
+          feed_id: sub.feed_id
+        })
+        .pluck('name')
+        .value();
+    });
+  };
+
+  var getSubscriptions = function(callback) {
+    request(self.apiBaseUrl + '/subscriptions.json', function(err, res, body) {
+      if (!err && res.statusCode === 200) {
+        var subs = JSON.parse(body);
+        callback(null, subs);
+      } else {
+        callback(err, null);
+      }
+    });
+  };
+
+  var getTags = function(callback) {
+    request(self.apiBaseUrl + '/taggings.json', function(err, res, body) {
+      if (!err && res.statusCode === 200) {
+        var tags = JSON.parse(body);
+        var tagNames = _.chain(tags)
+          .uniq(false, 'name')
+          .pluck('name')
+          .value();
+
+        var result = {
+          tags: tags,
+          tagNames: tagNames
+        };
+
+        callback(null, result);
+      } else {
+        callback(err, null);
+      }
+    });
+  };
+
+  self.init(ready);
 };
 
 module.exports = Feedbin;
