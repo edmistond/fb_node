1153a8d06ed27ce6412020ddc64745fe80da8e98
David Edmiston <edmistond@gmail.com>
2015-05-21 22:24:29 -0400

    Updating the test file a bit

--- test.js
+++ test.js
@@ -2,17 +2,19 @@ var Feedbin = require('./index');
 var _ = require('lodash');
 var async = require('async');
 
-var readyFunc = function() {
-  // console.log(fb.getSubcriptionsForTag('mac-tech').length);
-  // console.log(fb.getSubcriptionsForTag());
+var fb = new Feedbin('edmistond', 'pass', 'http://localhost:3002', function() {
+	var subs = fb.getSubcriptionsForTag('mac-tech');
+	_.forEach(subs, function(s) {
+		fb.deleteSubscription(s.id, function(err, res) {
+			if (err) console.log(err);
+			if (res) console.log(res, s.title);
+		});
+	});
 
-  _.forEach(fb.tagNames, function(t) {
-    console.log('current tag:', t);
-    _.forEach(fb.getSubcriptionsForTag(t), function(sub) {
-      console.log("\t", sub.title, sub.tags);
-    });
-    console.log("\n\n");
-  });
-};
-
-var fb = new Feedbin('edmistond', 'pass', 'http://localhost:3002', readyFunc);
+	console.log("getting untagged subs\n\n");
+	var untagged = fb.getSubcriptionsForTag('');
+	_.forEach(untagged, function(s) {
+		console.log(s.id, s.title);
+	});
+	console.log("\n");
+});
